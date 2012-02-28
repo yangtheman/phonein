b49281c475c903fb2617e6ca74ac8b2d68a0d948
Yang Chung <yangtheman@gmail.com>
2012-02-28 02:10:05 -0800

    Fix task list error

--- app/models/client.rb
+++ app/models/client.rb
@@ -4,11 +4,11 @@ class Client < ActiveRecord::Base
   belongs_to :admin
       
   def task_list
-    tasks = self.tasks rescue nil
+    tasks = self.tasks
     task_list = "There are #{tasks.size} tasks to perform today."
     if tasks.size > 0
       tasks.each_with_index do |task, index|
-        task_list << "#{index+1}, #{task}."
+        task_list << "Task #{index+1}, #{task.name}. "
       end
     end
     task_list
